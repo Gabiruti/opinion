c3e13a740d8c40a4ba76be2b53b554eed178972c
Gabiruti <gabrielrmeneses@outlook.com>
2025-07-17 01:54:32 -0300

    chore - add shadcn configs and esling rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,10 +7,17 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  
 });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      'no-console': 'warn',
+      "indent": ["error", 2]
+    }
+  }
 ];
 
 export default eslintConfig;
