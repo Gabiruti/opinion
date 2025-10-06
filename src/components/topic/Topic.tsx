aa2a8f26e723c2655e2d726f597aac6b4faa0998
Gabiruti <gabrielrmeneses@outlook.com>
2025-10-06 16:00:16 -0300

    refactor - removing ugly button

--- src/components/topic/Topic.tsx
+++ src/components/topic/Topic.tsx
@@ -1,6 +1,5 @@
 import {
   Card,
-  CardAction,
   CardContent,
   CardDescription,
   CardHeader,
@@ -11,23 +10,19 @@ import Link from "next/link";
 
 export default function Topic({ id, title, description, content }: ITopic) {
   return (
-    <Card className={`w-full`}>
-      <CardHeader>
-        <CardTitle>{title}</CardTitle>
-        <CardDescription>{description}</CardDescription>
-        <CardAction>
-          <Link
-            type="button"
-            className="px-2 py-1 bg-gray-500 text-white rounded cursor-pointer"
-            href={`/topic/${id}`}
-          >
-            View
-          </Link>
-        </CardAction>
-      </CardHeader>
-      <CardContent>
-        <p>{content}</p>
-      </CardContent>
-    </Card>
+    <Link
+      // className="px-2 py-1 bg-gray-500 text-white rounded cursor-pointer"
+      href={`/topic/${id}`}
+    >
+      <Card className={`w-full`}>
+        <CardHeader>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>{description}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>{content}</p>
+        </CardContent>
+      </Card>
+    </Link>
   );
 }
